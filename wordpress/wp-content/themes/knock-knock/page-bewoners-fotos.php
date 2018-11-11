b72edebb533376aa2c18e5a59c5b81f264d31b5f
Mike Verbruggen <mike@slmmr.nl>
2018-11-11 20:08:22 +0100

    Update wordpress/wp-content/themes/knock-knock/page-bewoners-fotos.php

--- wordpress/wp-content/themes/knock-knock/page-bewoners-fotos.php
+++ wordpress/wp-content/themes/knock-knock/page-bewoners-fotos.php
@@ -37,7 +37,7 @@
 					        // get all the user's data
 					        $author_info = get_userdata( $author->ID );
 							$phone = get_field('resident_phone', $author_info); ?>
-							<div style="disply:inline">
+							<div style="display:inline">
 							<!--<a href="<?php echo $author_info->user_nicename; ?>">-->
 							<img src="<?php the_field( 'resident_profile_image', $author_info ); ?>" width="94" height="94" alt="" />
 							<!--</a>-->
