19548f701c8fd5bd8d6e093c6e5448ca44de5645
Kasper Koman <kasper.koman@gmail.com>
2019-12-02 22:43:11 +0100

    Fix old-member role capabilities

--- wordpress/wp-content/themes/knock-knock/functions.php
+++ wordpress/wp-content/themes/knock-knock/functions.php
@@ -169,49 +169,45 @@ add_action( 'init', 'cptui_register_my_cpts' );
  * Add user old-member
  */
 
-add_role( 'old-member', __('Uitgeschreven' ),
- 
-array(
-'read' => true, // true allows this capability
-'edit_posts' => true, // Allows user to edit their own posts
-'edit_pages' => true, // Allows user to edit pages
-'edit_others_posts' => true, // Allows user to edit others posts not just their own
-'create_posts' => true, // Allows user to create new posts
-'manage_categories' => true, // Allows user to manage post categories
-'publish_posts' => true, // Allows the user to publish, otherwise posts stays in draft mode'edit_themes' => false, // false denies this capability. User can’t edit your theme
-'edit_files' => true,
-'edit_theme_options'=>true,
-'manage_options'=>true,
-'moderate_comments'=>true,
-'manage_categories'=>true,
-'manage_links'=>true,
-'edit_others_posts'=>true,
-'edit_pages'=>true,
-'edit_others_pages'=>true,
-'edit_published_pages'=>true,
-'publish_pages'=>true,
-'delete_pages'=>true,
-'delete_others_pages'=>true,
-'delete_published_pages'=>true,
-'delete_others_posts'=>true,
-'delete_private_posts'=>true,
-'edit_private_posts'=>true,
-'read_private_posts'=>true,
-'delete_private_pages'=>true,
-'edit_private_pages'=>true,
-'read_private_pages'=>true,
-'unfiltered_html'=>true,
-'edit_published_posts'=>true,
-'upload_files'=>true,
-'publish_posts'=>true,
-'delete_published_posts'=>true,
-'delete_posts'=>true,
-'install_plugins' => false, // User cant add new plugins
-'update_plugin' => false, // User can’t update any plugins
-'update_core' => false // user cant perform core updates
- 
-)
-);
+add_role( 'old-member', __('Uitgeschreven'), array(
+	'read' => true, // true allows this capability
+	'edit_posts' => false, // Allows user to edit their own posts
+	'edit_pages' => false, // Allows user to edit pages
+	'edit_others_posts' => false, // Allows user to edit others posts not just their own
+	'create_posts' => false, // Allows user to create new posts
+	'manage_categories' => false, // Allows user to manage post categories
+	'publish_posts' => false, // Allows the user to publish, otherwise posts stays in draft mode'edit_themes' => false, // false denies this capability. User can’t edit your theme
+	'edit_files' => false,
+	'edit_theme_options' => false,
+	'manage_options' => false,
+	'moderate_comments' => false,
+	'manage_categories' => false,
+	'manage_links' => false,
+	'edit_others_posts' => false,
+	'edit_pages' => false,
+	'edit_others_pages' => false,
+	'edit_published_pages' => false,
+	'publish_pages' => false,
+	'delete_pages' => false,
+	'delete_others_pages' => false,
+	'delete_published_pages' => false,
+	'delete_others_posts' => false,
+	'delete_private_posts' => false,
+	'edit_private_posts' => false,
+	'read_private_posts' => false,
+	'delete_private_pages' => false,
+	'edit_private_pages' => false,
+	'read_private_pages' => false,
+	'unfiltered_html' => false,
+	'edit_published_posts' => false,
+	'upload_files' => false,
+	'publish_posts' => false,
+	'delete_published_posts' => false,
+	'delete_posts' => false,
+	'install_plugins' => false, // User cant add new plugins
+	'update_plugin' => false, // User can’t update any plugins
+	'update_core' => false // user cant perform core updates
+));
 
 /**
  * Remove unused user roles
