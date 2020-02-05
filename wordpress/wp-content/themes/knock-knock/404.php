08426a6ab59bb0c8478ce888106007a3928ef23f
Kasper Koman <kasper.koman@gmail.com>
2020-02-05 19:55:11 +0100

    Added 404 page

---
+++ wordpress/wp-content/themes/knock-knock/404.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<section id="grid-system">
+
+    <div class="page-header">
+        <h1>Oh oh...</h1>
+    </div>
+
+    <div class="row">
+        <!-- Content -->
+        <div class="span8">
+
+            <p>Sorry, we konden deze pagina niet vinden!</p>
+
+        </div><!-- /Content -->
+
+    </div>
+
+</section>
+
+<?php get_footer(); ?>
