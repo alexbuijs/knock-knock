5a8bd36798c8e55b56b4df3f134df705df13df33
Kasper Koman <kasper.koman@gmail.com>
2020-02-18 15:03:48 +0100

    Added comment

--- wordpress/wp-content/mu-plugins/load.php
+++ wordpress/wp-content/mu-plugins/load.php
@@ -1,2 +1,7 @@
 <?php
+/**
+ * All files in the root of the mu-plugin folder get 
+ * loaded automatically by Wordpress but not the files
+ * inside folders, so we load them here
+ */
 require WPMU_PLUGIN_DIR . '/knock-knock-login-screen/knock-knock-login-screen.php';
