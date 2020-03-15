19bc408ce6d63ea1bc088dc0097bcfd9ba4a9197
Kasper Koman <kasper.koman@gmail.com>
2020-03-15 18:06:33 +0100

    Navbar responsiveness improvements

--- wordpress/wp-content/themes/knock-knock/partials/navbar.php
+++ wordpress/wp-content/themes/knock-knock/partials/navbar.php
@@ -1,35 +1,34 @@
 <nav class="navbar navbar-dark bg-primary navbar-expand-lg">
-    <div class="container">
+    <div class="container-lg">
         <a class="navbar-brand" href="<?=get_bloginfo('url')?>">
             <svg role="img" class="normal" title="<?=get_bloginfo('name')?>">
                 <use xlink:href="<?=get_template_directory_uri() . '/assets/images/logo.svg#logo'?>"/>
             </svg>
         </a>
+        <ul class="navbar-nav ml-auto d-lg-none">
+            <?php get_template_part('partials/navbar/user-badge'); ?>
+        </ul>
+
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu-toggle" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div id="menu-toggle" class="collapse navbar-collapse">
-        <?php wp_nav_menu([
-    'menu' => 'Hoofd',
-    'menu_class' => 'navbar-nav mr-auto',
-    'container' => false,
-    'depth' => 2,
-    'show_home' => '1']);?>
+            <?php 
+                wp_nav_menu([
+                    'menu' => 'Hoofd',
+                    'menu_class' => 'navbar-nav mr-auto',
+                    'container' => false,
+                    'depth' => 2,
+                    'show_home' => '1'
+                ]);
+            ?>
 
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <?=wp_get_current_user()->display_name?>
-                        <img class="user" src="<?=App\getUserImage()?> " alt=""/>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="<?= admin_url('profile.php'); ?>">Profiel</a>
-                        <a class="dropdown-item" href="<?= wp_logout_url(home_url()); ?>">Uitloggen</a>
-                    </div>
-                </li>
+            <ul class="navbar-nav ml-auto d-none d-lg-block">
+                <?php get_template_part('partials/navbar/user-badge'); ?>
             </ul>
         </div>
+
 
     </div>
 </nav>
---
+++ wordpress/wp-content/themes/knock-knock/partials/navbar/user-badge.php
@@ -0,0 +1,10 @@
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <?=wp_get_current_user()->first_name?> <span class="d-none d-lg-inline"><?=wp_get_current_user()->last_name?></span>
+        <img class="user" src="<?=App\getUserImage()?> " alt=""/>
+    </a>
+    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+        <a class="dropdown-item" href="<?= admin_url('profile.php'); ?>">Profiel</a>
+        <a class="dropdown-item" href="<?= wp_logout_url(home_url()); ?>">Uitloggen</a>
+    </div>
+</li>
