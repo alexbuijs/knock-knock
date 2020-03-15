b36581f19f498920166e2e2397b11eec48003e05
Kasper Koman <kasper.koman@gmail.com>
2020-03-15 18:44:06 +0100

    Header in correct place on 404 page

--- wordpress/wp-content/themes/knock-knock/404.php
+++ wordpress/wp-content/themes/knock-knock/404.php
@@ -1,10 +1,18 @@
 <?php get_header(); ?>
 
+<div class="row mb-3">
+    <div class="col-12">
+        <h3 class="font-weight-bold">
+            <i class="fas fa-fw fa-question text-muted"></i> 404
+        </h3>
+    </div>
+</div>
+
 <div class="row">
     <div class="col-12">
         <div class="card">
             <div class="card-header bg-transparent">
-                <h3  class="font-weight-bold"><i class="fas fa-fw fa-question text-muted"></i> 404</h3>
+
             </div>
             <div class="card-body mt-3">
                 <p>Oh oh, deze pagina konden we niet vinden!</p>
