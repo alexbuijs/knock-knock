f14a57ffaec5212ad318649c86f05e1faef561dc
Alex Buijs <abuijs@gitlab.com>
2020-03-15 18:19:29 +0100

    Always show document attachments

--- wordpress/wp-content/themes/knock-knock/partials/page-documentatie/downloads.php
+++ wordpress/wp-content/themes/knock-knock/partials/page-documentatie/downloads.php
@@ -1,24 +1,28 @@
-<?php if (have_rows('downloads')): ?>
-  <div class="card">
-    <div class="card-header bg-transparent">
-      <h5>
-        Bijlagen
-      </h5>
-    </div>
+<div class="card">
+  <div class="card-header bg-transparent">
+    <h5>
+      Bijlagen
+    </h5>
+  </div>
 
-    <div class="card-body mb-3">
-      <ul class='list-unstyled'>
+  <div class="card-body mb-3">
+    <ul class='list-unstyled'>
+      <?php if (!have_rows('downloads')): ?>
+        <div class="text-muted">
+          Geen bestanden
+        </div>
+      <?php else: ?>
         <?php while (have_rows('downloads')) : the_row(); ?>
           <?php $bestand = get_sub_field('bestand'); if ($bestand): ?>
             <li class='d-flex'>
-              <div>
+              <div class='mr-1'>
                 <i class="far fa-fw fa-file text-muted"></i>
               </div>
               <a href="<?php echo $bestand['url']; ?>"><?php the_sub_field('omschrijving'); ?></a>
             </li>
           <?php endif; ?>
         <?php endwhile; ?>
-      </ul>
-    </div>
+      <?php endif; ?>
+    </ul>
   </div>
-<?php endif; ?>
+</div>
