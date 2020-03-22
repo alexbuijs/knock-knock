ed218420a74706059c606a7c966ff0832fdc8990
Kasper Koman <kasper.koman@gmail.com>
2020-03-22 19:37:59 +0100

    Don't show current user image when user is not found

--- wordpress/wp-content/themes/knock-knock/page-bewoner.php
+++ wordpress/wp-content/themes/knock-knock/page-bewoner.php
@@ -7,7 +7,7 @@
         <div class="d-flex justify-content-between align-items-center">
             <h3 class="font-weight-bold">
                 <i class="far fa-fw fa-user text-muted"></i>
-                    <?= $user ? $user->display_name : 'Bewoner' ?>
+                    <?= $user ? $user->first_name . $user->last_name : 'Bewoner' ?>
             </h3>
             <a href="<?= get_bloginfo('url'); ?>/bewoners" class="btn btn-primary">Terug naar overzicht</a>
         </div>
@@ -15,18 +15,18 @@
 </div>
 
 <div class="row">
-    <div class="col-12 col-sm-6">
-        <div class="card p-3">
-            <div class="profile-picture" style="background-image:url('<?= App\getUserImage('large', $user->ID) ?>')"></div>
-        </div>
-    </div>
-    <div class="col-12 col-sm-6">
-        <div class="card">
-            <div class="card-header bg-transparent">
-
+    <?php if ($user) : ?>
+        <div class="col-12 col-sm-6">
+            <div class="card p-3">
+                <div class="profile-picture" style="background-image:url('<?= App\getUserImage('large', $user->ID) ?>')"></div>
             </div>
-            <div class="card-body">
-                <?php if ($user) : ?>
+        </div>
+        <div class="col-12 col-sm-6">
+            <div class="card">
+                <div class="card-header bg-transparent">
+
+                </div>
+                <div class="card-body">
                     <h5 class="font-weight-bold mb-3">
                         Info
                     </h5>
@@ -35,12 +35,18 @@
                     <?php if ($phone = get_field('resident_phone', $user)) : ?>
                         <p><i class="fas fa-phone fa-fw mr-2"></i><a href="tel:<?= $phone ?>"><?= $phone ?></a></p>
                     <?php endif; ?>
-                <?php else : ?>
-                    <p>Bewoner niet gevonden</p>
-                <?php endif; ?>
+                </div>
             </div>
         </div>
-    </div>
+    <?php else : ?>
+        <div class="col-12"> 
+            <div class="card">
+                <div class="card-body pt-3">
+                    <p>Bewoner niet gevonden</p>
+                </div>
+            </div>
+        </div>
+    <?php endif; ?>
 </div>
 
 <?php get_footer(); ?>
