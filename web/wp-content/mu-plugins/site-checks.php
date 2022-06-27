21a51c2ad2837c4c485459c985b022b3ac3d55ea
kapoko <17455539+kapoko@users.noreply.github.com>
2022-06-27 18:01:31 +0200

    Allow for automatic updates even under version control

--- web/wp-content/mu-plugins/site-checks.php
+++ web/wp-content/mu-plugins/site-checks.php
@@ -10,4 +10,7 @@ License:      GPL-3.0-or-later
 add_filter('site_status_tests', function($tests) {
     unset($tests['async']['background_updates']);
     return $tests;
-}); 
+});
+
+// Allow for automatic updates even under version control
+add_filter('automatic_updates_is_vcs_checkout', '__return_false', 1);
