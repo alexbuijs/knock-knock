ea64da4bc5a5ec7cd023f50553e2535f90f12a00
kapoko <17455539+kapoko@users.noreply.github.com>
2022-11-15 11:15:10 +0530

    Prevent some unnecessary wordpress css from loading

--- web/wp-content/mu-plugins/knock-knock-cleanup.php
+++ web/wp-content/mu-plugins/knock-knock-cleanup.php
@@ -44,6 +44,7 @@ add_action(
         wp_dequeue_style("wp-block-library");
         wp_dequeue_style("wp-block-library-theme");
         wp_dequeue_style("global-styles");
+        wp_dequeue_style("classic-theme-styles");
     },
     200,
 );
